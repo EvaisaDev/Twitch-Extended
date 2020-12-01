3cb94e3d317b5b91bafb4b191d910bdc724a15fb
EvaisaGiac <evagiacosa1@gmail.com>
2020-12-01 21:31:43 +0100

    Fix some broken stuff and add more translations!

--- twitch_extended/files/entities/animals/glitch/twitchy_player.lua
+++ twitch_extended/files/entities/animals/glitch/twitchy_player.lua
@@ -3,6 +3,12 @@ local x, y, rot = EntityGetTransform( entity_id )
 
 for _,entity in pairs( EntityGetInRadiusWithTag( x, y, 20, "player_unit" ) or {} ) do
 	ent_x, ent_y = EntityGetTransform(entity)
-	effect = EntityLoad("mods/twitch_extended/files/entities/animals/glitch/effect_twitchy.xml", ent_x, ent_y)
-	EntityAddChild(entity, effect)
+	twitch_effect = EntityGetWithName( "glitch_twitchy_effect" )
+	if twitch_effect == nil or twitch_effect == 0 then
+		effect = EntityLoad("mods/twitch_extended/files/entities/animals/glitch/effect_twitchy.xml", ent_x, ent_y)
+		EntityAddChild(entity, effect)
+	else
+		lifetime = EntityGetFirstComponent(twitch_effect, "LifetimeComponent")
+		ComponentSetValue2(lifetime, "kill_frame", GameGetFrameNum() + 60)
+	end
 end
